b13633fffc200c5dd94f8c4b47bf77556718cad7
Sandor Seres <sseres@code.hu>
2024-02-03 21:38:54 +0100

    Some update

--- streaming.py
+++ streaming.py
@@ -1,5 +1,6 @@
 #
 # A választ streaming módban küldi a kliensnek, így chat módban használható
+# https://platform.openai.com/docs/api-reference
 #
 from fastapi import FastAPI, HTTPException, BackgroundTasks
 from fastapi.responses import StreamingResponse
@@ -11,12 +12,16 @@ from typing import List
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 import time
+import logging
+
+logger = logging.getLogger(__name__)
+
 # Statikus fájlok könyvtárának csatolása
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# CORS middleware hozzáadása
+# CORS middleware hozzáadása    
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Engedélyezett források (biztonsági okokból szűkítsd le!)
@@ -25,8 +30,8 @@ app.add_middleware(
     allow_headers=["*"],  # Engedélyezett fejlécek
 )
 
-API_KEY = "sk-NH"  # your api key goes here
-MODEL = "gpt-4"
+API_KEY = "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"  # your api key goes here
+MODEL = "gpt-4-0125-preview"
 MAX_TOKENS = 500
 
 # Egy egyszerű modell a kérésekhez és válaszokhoz
@@ -40,11 +45,6 @@ class ResponseModel(BaseModel):
 
 headers = {"Authorization": f"Bearer {API_KEY}"}
 
-
-import logging
-
-logger = logging.getLogger(__name__)
-
 async def generate_response_stream(query: str):
     body = {
         "model": MODEL,
@@ -54,7 +54,7 @@ async def generate_response_stream(query: str):
         ],
         "max_tokens": MAX_TOKENS,
         "stream": True,
-        "temperature": 0.9,
+        "temperature": 0.1,
         "top_p": 1,
         "frequency_penalty": 0,
         "presence_penalty": 0,
@@ -64,7 +64,7 @@ async def generate_response_stream(query: str):
             if response.status_code != 200:
                 raise HTTPException(status_code=response.status_code, detail="Error from OpenAI API")
             async for chunk in response.aiter_bytes():
-                print(time.time_ns() ,chunk)
+                # print(time.time_ns() ,chunk)
                 yield chunk
 
 
